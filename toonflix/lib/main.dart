f9ff672c58a65416c6bf180618fa15884d779558
Park Doyoung <dkdlel1z@naver.com>
2023-07-16 14:48:05 +0900

    make UI challenge

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -10,24 +10,67 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      'Hey, Doyoung',
-                      style: TextStyle(
-                        color: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Doyoung',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w800,
+                        ),
                       ),
-                    ),
-                    Text('Welcome back'),
-                  ],
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 22,
                 ),
-              ],
-            )
-          ],
+              ),
+              Text(
+                '\$ 12,000',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [Container(
+                  /// buttons Section : 6:30
+                )],
+              )
+            ],
+          ),
         ),
       ),
     );
