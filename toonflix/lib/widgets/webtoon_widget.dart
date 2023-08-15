af5d45620fa29d7903652fee51f58fc3fa0ffc3f
Park Doyoung <dkdlel1z@naver.com>
2023-08-15 23:28:23 +0900

    web_view

--- toonflix/lib/widgets/webtoon_widget.dart
+++ toonflix/lib/widgets/webtoon_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/detail_screen.dart';
 
 class WebtoonList extends StatelessWidget {
   final String title, thumb, id;
@@ -12,8 +13,8 @@ class WebtoonList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // GetstureDetector : 클릭 이벤트
+    return GestureDetector(
+      // GestureDetector : 클릭 이벤트
       // 위젯으로 빼기 dart class
       // 다른 화면으로 보내기. 어떤 스크린 선택? ->
       // 모든 화면은 위젯. 화면전환 == 위젯바꿈
@@ -21,36 +22,51 @@ class WebtoonList extends StatelessWidget {
       // route != statelessWidget
       // routebuilder?
       // scoffold 부터 시작. appbar 다시 생성
-
-      children: [
-        const SizedBox(
-          height: 20,
-        ),
-        Container(
-          // padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  blurRadius: 10,
-                  offset: const Offset(3, 3),
-                )
-              ]),
-          height: 300,
-          child: Image.network(
-            thumb,
+      onTap: () {
+        Navigator.push(
+          // 애니메이션 효과
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                DetailScreen(title: title, thumb: thumb, id: id),
+            // fullscreenDialog: true,
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(title,
-            style: const TextStyle(
-              fontSize: 15,
-            )),
-      ],
+        );
+      },
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Hero(
+            tag: id,
+            child: Container(
+              // padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      blurRadius: 10,
+                      offset: const Offset(3, 3),
+                    )
+                  ]),
+              height: 300,
+              child: Image.network(
+                thumb,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(title,
+              style: const TextStyle(
+                fontSize: 15,
+              )),
+        ],
+      ),
     );
   }
 }
